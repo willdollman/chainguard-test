03e98b8715155076330a62b3c85c13490fc853fa
Will Dollman <will.dollman@sourcegraph.com>
2022-12-01 10:34:11 +0000

    Add SG image variant based on Wolfi repos

--- sourcegraph-chainguard-wolfi/build.sh
+++ sourcegraph-chainguard-wolfi/build.sh
@@ -3,8 +3,8 @@
 # Build base image using apko build container
 docker run -v "$PWD":/work \
     cgr.dev/chainguard/apko \
-    build apko.yaml sg-cg-alpine:latest sg-cg-alpine.tar ||
+    build apko.yaml sg-cg-wolfi:latest sg-cg-wolfi.tar ||
     echo "*** Build failed ***"
 
 # Import into Docker
-docker load <sg-cg-alpine.tar
+docker load <sg-cg-wolfi.tar
